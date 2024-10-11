3923cd1891195c93c71e5096166282c906ff49cc
Marian Halibei <137928134+marian2702@users.noreply.github.com>
2024-10-11 16:38:31 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -9,7 +9,7 @@ function fetchJSONData() {
         .then((data) => {
             const movieCards = document.getElementById('movie-cards');
             // Clear existing cards if necessary
-            
+            movieCards.innerHTML = '';
 
             // Loop through each movie and create a card
             data.moviesDatabase.forEach(movie => {
@@ -48,7 +48,7 @@ function fetchJSONData() {
 
                 // Add click event to redirect to test.html with MovieID
                 movieCard.addEventListener('click', () => {
-                    window.location.href = `test.html?movie.id=${movie.MovieID}`;
+                    window.location.href = `test.html?movieid=${movie.MovieID}`;
                 });
 
                 movieCards.appendChild(movieCard);
